193a89e495de1a8748931a973823c20302516338
leafgg <jobs.lugui@gmail.com>
2024-04-01 22:17:40 -0300

    feat: products stock graph using chart.js

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,7 +1,8 @@
+import { ChartData, ChartOptions } from 'chart.js';
 import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { Subject, pipe, takeUntil } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsService } from 'src/app/services/products/products.service';
 
@@ -13,6 +14,9 @@ import { ProductsService } from 'src/app/services/products/products.service';
 export class DashboardHomeComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   public productList: Array<GetAllProductsResponse> = [];
+
+  public productsChartDatas?: ChartData;
+  public productsChartOptions?: ChartOptions;
 
   constructor(
     private productsService: ProductsService,
@@ -32,6 +36,7 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
           if (response.length > 0) {
             this.productList = response;
             this.productsDtService.setProductsDatas(this.productList);
+            this.setProductsChartConfig();
           }
         },
         error: (err) => {
@@ -44,6 +49,64 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
           });
         },
       });
+  }
+
+  setProductsChartConfig(): void {
+    if (this.productList.length > 0) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue(
+        '--text-color-secondary'
+      );
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.productsChartDatas = {
+        labels: this.productList.map((item) => item?.name),
+        datasets: [
+          {
+            label: 'Quantity',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor:
+              documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productList.map((item) => item?.amount),
+          },
+        ],
+      };
+
+      this.productsChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: '500',
+              },
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    }
   }
   ngOnDestroy(): void {
     this.destroy$.next();
