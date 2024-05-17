8abb9ed433a14771e322c3f2d2a9bb40dc211984
leafgg <jobs.lugui@gmail.com>
2024-05-17 04:40:11 -0300

    feat: preloadingStrategy included

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { AuthGuard } from './guards/auth-guard.service';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
@@ -42,7 +42,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
